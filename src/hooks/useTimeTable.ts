e5e3484d42ff33a46e1fbfdc7a86e21a74df0ce1
yonghwanjung <yhjung1231@bythem.net>
2024-09-26 14:50:07 +0900

    수업시간 유효성 검사, 수업추가 시 시간 설정 고도화

--- src/hooks/useTimeTable.ts
+++ src/hooks/useTimeTable.ts
@@ -9,6 +9,13 @@ type TParams = {
   isApplyToAll: boolean
 }
 
+const messages = {
+  0: '시작시간은 종료시간보다 늦을 수 없습니다.',
+  1: '시작시간은 이전 교시의 종료시간보다 이를 수 없습니다.',
+  2: '종료시간은 시작시간보다 이를 수 없습니다.',
+  3: '종료시간은 다음 교시의 시작시간보다 늦을 수 없습니다.',
+}
+
 export const useTimeTable = ({
   classroomInfo,
   setClassrooms,
@@ -46,12 +53,27 @@ export const useTimeTable = ({
 
   const addPeriod = (partIdx: number) => {
     updateParts((parts) => {
-      const target = parts[partIdx]
-      if (target.periods.length >= 5) {
+      const currPart = parts[partIdx]
+      if (currPart.periods.length >= 5) {
         return alert('최대 5개까지 등록 가능합니다.')
       }
-      target.periods.push({ start: '', end: '' })
+      let setTime = { start: '00:00', end: '00:00' }
+      for (let i = partIdx; i >= 0; i--) {
+        if (parts[i].periods.length > 0) {
+          const lastPeriod = [...parts[i].periods].pop()
+          const { end } = lastPeriod as { end: string }
+          setTime = { start: end, end }
+          break
+        }
+      }
+      currPart.periods.push(setTime)
     })
+  }
+
+  const flattenPeriods = () => {
+    if (!classroomInfo.data?.parts) return []
+    const periods = classroomInfo.data.parts.map((part) => part.periods)
+    return periods.flat()
   }
 
   const updatePeriod = (
@@ -62,8 +84,31 @@ export const useTimeTable = ({
     const value = e.target.value
     const timeType = e.target.dataset.type as 'start' | 'end'
     updateParts((parts) => {
-      const target = parts[partIdx]
-      target.periods[periodIdx][timeType] = value
+      const currPart = parts[partIdx]
+      const currPeriod = currPart.periods[periodIdx]
+      const allPeriods = flattenPeriods()
+      const currPeriodIdx = allPeriods.findIndex(
+        ({ number }) => number === currPeriod.number,
+      )
+      const prevPeriod = allPeriods[currPeriodIdx - 1]
+      const nextPeriod = allPeriods[currPeriodIdx + 1]
+      const startAfterEnd = value > currPeriod.end
+      const startBeforePrevEnd = prevPeriod && value < prevPeriod.end
+      const endBeforeStart = value < currPeriod.start
+      const endAfterNextStart = nextPeriod && value > nextPeriod.start
+      switch (timeType) {
+        case 'start': {
+          if (startAfterEnd) return alert(messages['0'])
+          if (startBeforePrevEnd) return alert(messages['1'])
+          break
+        }
+        case 'end': {
+          if (endBeforeStart) return alert(messages['2'])
+          if (endAfterNextStart) return alert(messages['3'])
+          break
+        }
+      }
+      currPeriod[timeType] = value
     })
   }
 
